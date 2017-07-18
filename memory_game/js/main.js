679a3cc8e0016ebcd882f0be5f7b69b539a357c7
Bluhm <bpc127@dca9046e3ec4.cof.ds.capitalone.com>
2017-07-18 11:09:56 -0400

    added more logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 
 
 
-var cardOne = "Queen";
-var cardTwo = "King"
-var cardThree = "Queen";
-var cardFour = "King"
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
+
+
+var cardOne = cards.pop();
 
 console.log("User flipped: " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardOne);
+
+console.log("User flipped: " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardTwo === cardOne) {
+    alert('You found a match!');
+  } else {
+    alert('You did not find a match, sorry.');
+  }
+}
