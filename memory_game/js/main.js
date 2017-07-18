50edfc7572aa0526db84df33dc3a2f8250e5e3aa
Bluhm <bpc127@dca9046e3ec4.cof.ds.capitalone.com>
2017-07-18 14:14:27 -0400

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,21 +5,22 @@ var cards = ["king", "king", "queen", "queen"];
 var cardsInPlay = [];
 
 
-var cardOne = cards.pop();
-
-console.log("User flipped: " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-cardsInPlay.push(cardOne);
-
-console.log("User flipped: " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardTwo === cardOne) {
-    alert('You found a match!');
-  } else {
-    alert('You did not find a match, sorry.');
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert('You found a match!');
+    } else {
+      alert('You did not find a match, sorry.');
+    }
   }
+}
+
+
+var flipCard = function(cardID) {
+  cardsInPlay.push(cards[cardID]);
+  console.log("User flipped: " + cards[cardID]);
+
+  cardsInPlay.push(cards[cardID]);
+
+  checkForMatch();
 }
