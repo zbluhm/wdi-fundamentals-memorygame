92cc08223f7a38f8e08571f434bf1619ed00e8ee
Bluhm <bpc127@dca9046e3ec4.cof.ds.capitalone.com>
2017-07-19 13:42:08 -0400

    final deliverable

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,6 +31,8 @@ var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert('You found a match!');
+      var score = document.getElementById('score').textContent.charAt(7);
+      document.getElementById('score').textContent = "Score: " + (parseInt(score)+1);
     } else {
       alert('You did not find a match, sorry.');
     }
@@ -38,16 +40,33 @@ var checkForMatch = function() {
 }
 
 
-var flipCard = function(cardID) {
+var flipCard = function() {
+  var cardID = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardID].rank);
   console.log("User flipped: " + cards[cardID].rank);
   console.log(cards[cardID].cardImage);
   console.log(cards[cardID].suit);
-
-  cardsInPlay.push(cards[cardID].rank);
-
+  this.setAttribute('src', cards[cardID].cardImage);
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var imgItem = document.createElement('img');
+    imgItem.setAttribute('src', "images/back.png");
+    imgItem.setAttribute('data-id', i);
+    imgItem.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(imgItem);
+  }
+}
+
+var resetBoard = function() {
+  cardsInPlay = [];
+  var cards = document.getElementsByTagName('img');
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].setAttribute('src', "images/back.png");
+  }
+}
+
+document.getElementsByName('button')[0].addEventListener('click', resetBoard);
+createBoard();
